6b30ad22d7514590cddf426bd4372c2df2f7af6e
wuqinqiang <1185079673@qq.com>
2023-02-15 23:30:08 +0800

    feat: add bbdc collector

---
+++ collector/collector.go
@@ -0,0 +1,4 @@
+package collector
+
+type Collector interface {
+}
---
+++ tools/restry.go
@@ -0,0 +1,25 @@
+package tools
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+var (
+	Resty *resty.Client
+
+	defaultClient = &http.Client{
+		Timeout: 6 * time.Second,
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+			MaxIdleConns:      15,
+			IdleConnTimeout:   90 * time.Second,
+		},
+	}
+)
+
+func init() {
+	Resty = resty.NewWithClient(defaultClient)
+}
